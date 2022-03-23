7a7f379ae3813f972fde2a7630a7e2d8cbd114c3
Simon-Xu-COMP <21096101d@connect.polyu.hk>
2022-03-23 19:28:20 +0800

    fdafaedfe

--- copy.cpp
+++ copy.cpp
@@ -1,7 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
-char a[100];
+
 int main() {
-	scanf("%s", a + 1);
-	
+	string a;
+	cin >> a;
+	cout << "zf: zero or not " << endl;
+	cout << "sf: negative or not " << endl;
+	cout << "of: signed overflow " << endl;
+	if (a == "jmp") {
+		cout << "direct jump" << endl;
+	}
+	if (a == "je") {
+		printf("zf 0 res 0\n");
+		printf("zf 1 res 1\n");
+	}
+	if (a == "jne") {
+		printf("zf 0 res 1\n");
+		printf("zf 1 res 0\n");
+	}
+	if (a == "js") {
+		printf("sf 0 res 0\n");
+		printf("sf 1 res 1\n");
+	}
+	if (a == "jns") {
+		printf("sf 0 res 1\n");
+		printf("sf 1 res 0\n");
+	}
+	if (a == "jg") {
+		for (int i = 0; i <= 7; i++) {
+			bitset<3> p(i);
+			bitset<1> sf((i >> 2) & 1);
+			bitset<1> of((i >> 1) & 1);
+			bitset<1> zf(i & 1);
+			cout << "sf " << sf << " of " << of << " zf " << zf << " res " << (~(sf^of)&~zf) << endl;
+		}
+	}
+	if (a == "jge") {
+		for (int i = 0; i <= 3; i++) {
+			bitset<2> p(i);
+			bitset<1> sf((i >> 1) & 1);
+			bitset<1> of(i & 1);
+			cout << " sf " << sf << " of " << of << " res " << (~(sf ^ of)) << endl;
+		}
+	}
+	if (a == "jl") {
+		for (int i = 0; i <= 3; i++) {
+			bitset<2> p(i);
+			bitset<1> sf((i >> 1) & 1);
+			bitset<1> of(i & 1);
+			cout << " sf " << sf << " of " << of << " res " << (sf ^ of) << endl;
+		}
+	}
+	if (a == "jle") {
+		for (int i = 0; i <= 7; i++) {
+			bitset<3> p(i);
+			bitset<1> sf((i >> 2) & 1);
+			bitset<1> of((i >> 1) & 1);
+			bitset<1> zf(i & 1);
+			cout << "sf " << sf << " of " << of << " zf " << zf << " res " << ((sf^of)|zf) << endl;
+		}
+	}
 }
